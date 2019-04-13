d477b4e856f1b1c4c3d7ab98d34b9f6a701edf7e
Joel 🐙  Cornett <joel.cornett@gmail.com>
2019-04-12 22:50:15 -0400

    working

--- App.js
+++ App.js
@@ -1,29 +1,78 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- * @flow
- */
-
-import React, {Component} from 'react';
-import {Platform, StyleSheet, Text, View} from 'react-native';
+// @flow
+import React, { Component } from "react";
+import { Platform, StyleSheet, Text, View } from "react-native";
+import { BleManager } from "react-native-ble-plx";
 
 const instructions = Platform.select({
-  ios: 'Press Cmd+R to reload,\n' + 'Cmd+D or shake for dev menu',
+  ios: "Press Cmd+R to reload,\n" + "Cmd+D or shake for dev menu",
   android:
-    'Double tap R on your keyboard to reload,\n' +
-    'Shake or press menu button for dev menu',
+    "Double tap R on your keyboard to reload,\n" +
+    "Shake or press menu button for dev menu"
 });
 
 type Props = {};
-export default class App extends Component<Props> {
+
+type State = {
+  messages: string[],
+  devices: { [string]: boolean }
+};
+
+export default class App extends Component<Props, State> {
+  bleManager: *;
+
+  state = {
+    messages: [],
+    devices: {}
+  };
+
+  logMessage = (msg: string) => {
+    this.setState(prevState => ({
+      messages: [msg, ...prevState.messages]
+    }));
+  };
+
+  addDevice = (name: string) => {
+    this.setState(prevState => ({
+      devices: { ...prevState.devices, [name]: true }
+    }));
+  };
+
+  componentDidMount() {
+    this.bleManager = new BleManager();
+    this.bleManager.onStateChange(state => {
+      this.logMessage(state);
+      if (state === "PoweredOn") {
+        this.bleManager.startDeviceScan(
+          null,
+          { allowDuplicates: true },
+          (error, device) => {
+            if (error) {
+              this.logMessage(error);
+              return;
+            }
+            if (device.name != null) {
+              Array.from(Object.keys(device)).forEach(k => this.addDevice(k));
+              this.addDevice(device.name);
+              this.addDevice(device.id);
+            }
+          }
+        );
+      }
+    });
+  }
+
   render() {
     return (
       <View style={styles.container}>
-        <Text style={styles.welcome}>Welcome to React Native!</Text>
+        <Text style={styles.welcome}>Welcome to xyzReact Native!</Text>
         <Text style={styles.instructions}>To get started, edit App.js</Text>
         <Text style={styles.instructions}>{instructions}</Text>
+        <Text style={styles.instructions}>
+          {this.state.messages.join("\n")}
+        </Text>
+        <Text style={styles.instructions}>
+          {Array.from(Object.keys(this.state.devices)).join("\n")}
+        </Text>
       </View>
     );
   }
@@ -32,18 +81,18 @@ export default class App extends Component<Props> {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: "#F5FCFF"
   },
   welcome: {
     fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
+    textAlign: "center",
+    margin: 10
   },
   instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
+    textAlign: "center",
+    color: "#333333",
+    marginBottom: 5
+  }
 });
---
+++ ios/LibPlacholder.swift
@@ -0,0 +1,9 @@
+//
+//  LibPlacholder.swift
+//  bindu
+//
+//  Created by Joel Cornett on 4/12/19.
+//  Copyright © 2019 Facebook. All rights reserved.
+//
+
+import Foundation
