b407957c853930ac9b3ffe64cb6db48cfaeeffd4
Joel 🐙  Cornett <joel.cornett@gmail.com>
2019-04-12 21:16:37 -0400

    reset

--- components/BluetootherWidget.js
+++
@@ -1,9 +0,0 @@
-import React, { Component } from "react";
-import { BleManager } from "react-native-ble-plx";
-
-export default class BluetootherWidget extends Component {
-  constructor(props) {
-    super(props);
-    this.bleManager = new BleManager();
-  }
-}
--- ios/AppPlaceholder.swift
+++
@@ -1,9 +0,0 @@
-//
-//  AppPlaceholder.swift
-//  how-do-i-react-native-
-//
-//  Created by Joel Cornett on 4/6/19.
-//  Copyright © 2019 650 Industries, Inc. All rights reserved.
-//
-
-import Foundation
